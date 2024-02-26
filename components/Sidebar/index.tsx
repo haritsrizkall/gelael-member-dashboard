afccf3320534305f1c195e7161aeab42c28ebbb2
haritsrizkall <haritsrizkal@mail.ugm.ac.id>
2024-02-26 22:49:52 +0700

    change sidebar logo

--- components/Sidebar/index.tsx
+++ components/Sidebar/index.tsx
@@ -71,12 +71,7 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
       {/* <!-- SIDEBAR HEADER --> */}
       <div className="flex items-center justify-between gap-2 px-6 py-5.5 lg:py-6.5">
         <Link href="/dashboard/">
-          <Image
-            width={176}
-            height={32}
-            src={"/images/logo/logo.svg"}
-            alt="Logo"
-          />
+          <h1 className="font-bold text-white text-2xl">Gelael Member</h1>
         </Link>
 
         <button
@@ -105,7 +100,7 @@ const Sidebar = ({ sidebarOpen, setSidebarOpen }: SidebarProps) => {
 
       <div className="no-scrollbar flex flex-col overflow-y-auto duration-300 ease-linear">
         {/* <!-- Sidebar Menu --> */}
-        <nav className="mt-5 py-4 px-4 lg:mt-9 lg:px-6">
+        <nav className="py-4 px-4 lg:px-6">
           {/* <!-- Menu Group --> */}
           <div>
             <h3 className="mb-4 ml-4 text-sm font-semibold text-bodydark2">
