91f46b9182d0b3ef93682c729cfdf60dbcdbbde0
haritsrizkall <haritsrizkal@mail.ugm.ac.id>
2024-03-12 09:41:51 +0700

    Update page metadata and add redirect to dashboard

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,14 @@
 import { Metadata } from "next";
+import { redirect } from "next/navigation";
 
 export const metadata: Metadata = {
-  title: "TailAdmin | Next.js E-commerce Dashboard Template",
-  description: "This is Home Blog page for TailAdmin Next.js",
+  title: "Gelael Member Dashboard",
+  description: "Gelael Member Dashboard",
   // other metadata
 };
 
 export default function Home() {
+  redirect("/dashboard");
   return (
     <>
       
